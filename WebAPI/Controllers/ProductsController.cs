d8be0cb06210cd68ff287345baf3c3a41a7a750d
esrasnck <esrasancak@hotmail.com>
2021-02-14 22:47:27 +0300

    API eklendi

---
+++ WebAPI/Controllers/ProductsController.cs
@@ -0,0 +1,68 @@
+﻿using Business.Abstract;
+using Business.Concrete;
+using Core.Utilities.Results;
+using DataAccess.Concrete.EntityFramework;
+using Entities.Concrete;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private IProductService _productService;
+        // ınterface'in elinde somut referans yok. bunun için IoC yapısı gerek -- inversion of control => değişimin kontrolü
+        public ProductsController(IProductService productService)  // gevşek bağımlılık. Loosely coupled. bir bağımlılık var. ama soyuta  
+        {                                               //bağımlı.servis değişirse manager değişirsei herhangi bir problemle karşılaşmayacağız.
+            _productService = productService;
+        }
+
+        // Bazı projelerde managerlar çoğul hale gelebilir. hiç bir zaman bir katman diğer katmanın somutunu interface olmayanlar dışında bağlantı kuramazsın. 
+
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            // dependency chain (bağımlılık zinciri. Iproduct servce productmanager' o da ProductDal'a bağımlı. bu bağımlılığın çözülmesi için constructor injection yapılması gerek
+       
+            IDataResult<List<Product>> result = _productService.GetAll();
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        
+        // naming Convention => isimlendirme standartı.
+
+        // çözümlemek, interface'e bağlı olan sınıfı newlemek demek.
+
+       [HttpPost]
+       public IActionResult Post(Product product)
+        {
+            IResult result = _productService.Add(product);
+            if (result.Success)
+            {
+                return Ok(result);
+            }
+            return BadRequest(result);
+        }
+
+        [HttpGet]
+        public IActionResult GetById(int id)
+        {
+            IDataResult<Product> result = _productService.GetByID(id);
+            if (result.Success)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+    }
+}
