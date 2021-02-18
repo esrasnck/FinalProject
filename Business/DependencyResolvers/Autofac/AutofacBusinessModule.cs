56e26dd40c051e7904402499cc4186591b06f67f
Esra S <esrasancak@hotmail.com>
2021-02-18 17:29:08 +0300

    Devamı gelecek...

---
+++ Business/DependencyResolvers/Autofac/AutofacBusinessModule.cs
@@ -0,0 +1,34 @@
+﻿using Autofac;
+using Business.Abstract;
+using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.DependencyResolvers.Autofac
+{
+    public class AutofacBusinessModule: Module
+    {
+        protected override void Load(ContainerBuilder builder) // uygulama hayata geçtiği zaman, yayınlandığı zaman, uygulama ayağa kalktığı zaman çalışan metot
+        {
+            // bu alttaki metot, start up dosyasında yaptığımız services kısmına karşılık geliyor.
+            // biri senden IProductService isterse,  sen ona ProductManageri newleyip ver demek.
+            builder.RegisterType<ProductManager>().As<IProductService>().SingleInstance();
+            builder.RegisterType<EfProductDal>().As<IProductDal>().SingleInstance();
+
+            builder.RegisterType<CategoryManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EfCategoryDal>().As<ICategoryDal>().SingleInstance();
+
+            builder.RegisterType<OrderManager>().As<IOrderService>().SingleInstance();
+            builder.RegisterType<EfOrderDal>().As<IOrderDal>().SingleInstance();
+
+            builder.RegisterType<CustomerManager>().As<ICategoryService>().SingleInstance();
+            builder.RegisterType<EFCustomerDal>().As<ICustomerDal>().SingleInstance();
+        }
+
+
+
+    }
+}
--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,3 +1,4 @@
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
@@ -18,6 +19,7 @@ namespace WebAPI
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+            .UserServiceProviderFactory(new AutofacServiceProviderFactory())
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
